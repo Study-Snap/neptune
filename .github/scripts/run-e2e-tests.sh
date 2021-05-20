9ea50c7e4a6de9f5fdc986d637f22c448e2bb638
SystemFiles <bensykes12@gmail.com>
2021-05-20 16:40:09 -0400

    (SSPP-188): ES Health Check to trigger extra wait if needed

--- .github/scripts/run-e2e-tests.sh
+++ .github/scripts/run-e2e-tests.sh
@@ -6,11 +6,14 @@ set -e pipefail
 # wait for services to be available (elasticsearch primarily)
 .github/scripts/service-check.sh localhost:9200 -s -t 30 -- echo "Elasticsearch service is up!"
 
+if [ curl "http://localhost:9200/_cat/health" >/dev/null 2>&1 ]; then
+  sleep 15
+fi
+
 if [ $? -ne 0 ]; then
   echo "Elasticsearch service apparently never started... cannot continue."
   exit 2
 fi
 
 # if everything is good we can start the tests
-sleep 15
 npm run test:e2e
