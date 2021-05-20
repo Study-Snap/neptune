82c4992915141c63f51a0be1a1329b97d71f036b
SystemFiles <bensykes12@gmail.com>
2021-05-20 16:51:57 -0400

    (SSPP-188): ...

--- .github/scripts/run-e2e-tests.sh
+++ .github/scripts/run-e2e-tests.sh
@@ -6,9 +6,12 @@ set -e pipefail
 # wait for services to be available (elasticsearch primarily)
 .github/scripts/service-check.sh localhost:9200 -s -t 30 -- echo "Elasticsearch service is up!"
 
-if [ ! curl "http://localhost:9200/_cat/health" >/dev/null 2>&1 ]; then
-  sleep 15
-fi
+# check es cluster health (req: green)
+curl "http://localhost:9200/_cat/health"
+while [ $? -ne 0 ]; do
+  sleep 5
+  curl "http://localhost:9200/_cat/health"
+done
 
 if [ $? -ne 0 ]; then
   echo "Elasticsearch service apparently never started... cannot continue."
