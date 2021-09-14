0a39ac385abafba32e4e3ea98d19e800a34d9424
SystemFiles <bensykes12@gmail.com>
2021-09-14 10:46:08 -0400

    (CD): Implement service wait for authentication so it should always work

--- .github/scripts/run-e2e-tests.sh
+++ .github/scripts/run-e2e-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # run e2e tests with services enabled
 
-# wait for services to be available (elasticsearch primarily)
+# wait for services to be available (elasticsearch)
 .github/scripts/service-check.sh localhost:9200 -s -t 30 -- echo "Elasticsearch service is up!"
 
 # check es cluster health (req: green)
@@ -16,6 +16,9 @@ if [ $? -ne 0 ]; then
   echo "Elasticsearch service apparently never started... cannot continue."
   exit 2
 fi
+
+# wait for services to be available (authentication)
+.github/scripts/service-check.sh localhost:5555 -s -t 30 -- echo "Authentication service is running"
 
 # if everything is good we can start the tests
 npm run test:e2e
