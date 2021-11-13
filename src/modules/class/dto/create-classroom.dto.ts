989fc78c6bd45d11c63b3d564ececc87728ee610
SystemFiles <bensykes12@gmail.com>
2021-11-13 11:17:13 -0500

    (SSPP-300): Fixed validation issues for classroom creation + additional note validations

--- src/modules/class/dto/create-classroom.dto.ts
+++ src/modules/class/dto/create-classroom.dto.ts
@@ -1,9 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger'
-import { IsOptional, IsString } from 'class-validator'
+import { IsOptional, IsString, MaxLength, MinLength } from 'class-validator'
 
 export class CreateClassroomDto {
 	@ApiProperty({ default: 'Science 100', required: true })
 	@IsString({ message: 'Must include a name for the classroom' })
+	@MinLength(5, { message: 'Name must be at least 5 characters long' })
+	@MaxLength(25, { message: 'Name must not be more than 25 characters long' })
 	name: string
 
 	@ApiProperty({ default: 'classthumb.jpg', required: false })
--- src/modules/notes/dto/create-note.dto.ts
+++ src/modules/notes/dto/create-note.dto.ts
@@ -1,22 +1,24 @@
 import { ApiProperty } from '@nestjs/swagger'
-import { ArrayMinSize, IsArray, IsNotEmpty, IsOptional, IsString } from 'class-validator'
+import { ArrayMinSize, IsArray, IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from 'class-validator'
 
 export class CreateNoteDto {
 	@ApiProperty({ default: 'Lecture 5', required: true })
-	@IsNotEmpty({ message: 'You must enter a title' })
+	@MinLength(5, { message: 'Note title must be at least 5 characters long' })
+	@MaxLength(25, { message: 'Note title must be less than 25 characters long' })
 	@IsString()
 	title: string
 
-	@ApiProperty({ default: '91fasffsa48-42fas14-42fsaf1.pdf', required: true })
+	@ApiProperty({ default: 'XXXXXXXX-XXXXXXXXXX-XXXXXX.pdf', required: true })
 	@IsNotEmpty({ message: 'Must include a fileUri' })
 	@IsString({ message: 'FileUri must be a string' })
 	fileUri: string
 
 	@ApiProperty({ default: 'A note about lecture 5 in biology and math', required: true })
-	@IsNotEmpty({
+	@MinLength(60, {
 		message:
-			'Must enter a description for the file. We use this to help find your note. Hint: Pass a "shortDescription" in body of your request'
+			'Short description must be at least 60 characters long and provide some detail to the contents of the note.'
 	})
+	@MaxLength(120, { message: 'Short description must be less than 120 characters long' })
 	@IsString()
 	shortDescription: string
 
@@ -25,7 +27,7 @@ export class CreateNoteDto {
 	@ArrayMinSize(2, { message: 'Must specify at least 2 keywords' })
 	keywords: string[]
 
-	@ApiProperty({ default: 'dwa8dya8dwad-dw6ada7wd-gg21277dhd76h', required: true })
+	@ApiProperty({ default: 'XXXXXXXXXXXX-XXXXXXXXXX-XXXXXXX-XXXXXX', required: true })
 	@IsString()
 	@IsNotEmpty()
 	classId: string
